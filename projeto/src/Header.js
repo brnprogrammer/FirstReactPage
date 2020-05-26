668af5b535d9e98e3a60e2a80dbc390780fe14df
brnprogrammer <brunogithub@gmail.com>
2020-05-26 01:32:10 -0300

    Adicionando projeto ao repositorio

---
+++ projeto/src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+        <nav>
+        <div class="nav-wrapper indigo lighten-2">
+          <a href="#" class="brand-logo">Web Development</a>
+          <ul class="right">
+            <li><a href="#">Sass</a></li>
+            <li><a href="#">Components</a></li>
+            <li><a href="#">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+}
+export default Header;
